e0e93a8ea340bf6e7bcec9a28a8ec4f94edeb78c
TmCrafz <sterzeltim@gmail.com>
2017-09-03 14:52:50 +0200

    Count resized images

--- resize.sh
+++ resize.sh
@@ -18,7 +18,9 @@ if [[ -n "$logFile" ]]; then
 fi
 
 # Count of all files in source dir
-fileCnt="$(find $sourceDir -type f | wc -l)"
+totalFileCnt="$(find $sourceDir -type f | wc -l)"
+# Count how many files was converted
+convertedCnt=0
 # Count how many files was checked
 cnt=1
 # Loop over files in source dir
@@ -28,7 +30,7 @@ do
 	basename="$(basename $filename)"
 	# Only convert files
 	if [ -f "$filename" ]; then
-		echo "($cnt/$fileCnt) $filename"
+		echo "($cnt/$totalFileCnt) $filename"
 		(( cnt++ ))
 		width="$(identify -format '%w' $filename)"
 		height="$(identify -format '%h' $filename)"
@@ -36,6 +38,7 @@ do
 			# Resize image (with ImageMagick) to a min length of the given min size of 
 			# the smalles site and keep aspect ratio
 			convert $filename -resize "$minSize"x"$minSize"^ $destDir/$basename
+			(( convertedCnt++ ))
 			printf "\t \033[0;32mConverted\033[0m\n"
 			# Store name of converted file in log file if a log file was specified
 			if [[ -n "$logFile" ]]; then
@@ -44,3 +47,4 @@ do
 		fi
 	fi
 done
+echo "$convertedCnt of $totalFileCnt images resized"
